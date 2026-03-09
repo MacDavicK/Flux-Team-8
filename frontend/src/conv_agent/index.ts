9575f514a495b1b88b7e68201ef61db65449acb6
Harshal Kale <harshalsunilkale@gmail.com>
2026-03-10 02:23:20 +0400

    cleanup frontend

--- frontend/src/conv_agent/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Flux Conv Agent -- Barrel Export
- *
- * Re-exports the public API of the conv_agent module.
- */
-
-export type { VoiceMessage, VoiceStatus } from "./types";
-export { useVoiceAgent } from "./useVoiceAgent";
